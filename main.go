e9e92c7d97209aaea8208ea1e3336a679911fb6f
wavemomo <wave.mo@smartmore.com>
2021-09-17 11:37:23 +0800

    feat: print rebase

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+ * Author:       wavemomo
+ *
+ * START DATE:   2021/9/17 上午11:36
+ *
+ * CONTACT:      wave.mo@smartmore.com
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("rebase")
+}
